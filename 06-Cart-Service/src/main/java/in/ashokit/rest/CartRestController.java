9cb2805f1389a4d109f0b629a8f01f3333e19939
rushikesh052 <rushikeshshelke052@gmail.com>
2024-06-17 21:57:10 +0530

    'import statement changed'

--- 06-Cart-Service/src/main/java/in/ashokit/rest/CartRestController.java
+++ 06-Cart-Service/src/main/java/in/ashokit/rest/CartRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ashokit.constants.AppConstants;
@@ -17,7 +18,7 @@ import in.ashokit.dto.CartDto;
 import in.ashokit.props.AppProps;
 import in.ashokit.response.ApiResponse;
 import in.ashokit.service.CartService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 public class CartRestController {
